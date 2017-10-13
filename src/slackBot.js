83385766a9cce061380fd762d9b597218c275193
Yash Vora <yvora@ncsu.edu>
2017-10-12 20:33:45 -0400

    Reviewed the code and added some comments

--- src/slackBot.js
+++ src/slackBot.js
@@ -1,18 +1,15 @@
-
+// Importing the 'botkit' library
 var Botkit = require('botkit');
-
-
 var controller = Botkit.slackbot({
-  debug: false
-  
+  debug: false  
 });
 
-// connect the bot to a stream of messages
+// Connect the Bot to a stream of messages
 controller.spawn({
   token: process.env.PROMANTOKEN,
 }).startRTM()
 
-// give the bot something to listen for.
+// Initialize the Bot and provide it something to listen and respond.
 
 controller.hears('hello', 'direct_message', function(bot, message) 
 {
