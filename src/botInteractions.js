0a24259ec727dd9b4d9556a904120d7ac5aa148a
dgupta9 <dgupta9@ncsu.edu>
2017-10-21 13:10:45 -0400

    resolved issue #12

--- src/botInteractions.js
+++ src/botInteractions.js
@@ -19,7 +19,26 @@ var fetchCardHandler=function(convo, cardName, cardList){
     var count = cardList.length;
     for(var i=0;i<count;i++){
         if(cardName == cardList[i].name){
-           return;
+          convo.say("Here is the card "+cardName+" with description : "+cardList[i].desc);
+          convo.ask("What do you want to do ? Below are the available options:\n1) Add todo item\n2) Mark a todo item\n3) Remove a todo item",[
+              {
+                  pattern: "Add todo item",
+                  callback: function(){}// Function to handle add todo item
+              },
+              {
+                  pattern: "List todo items",
+                  callback: function(){}// Function to handle add todo item
+              },
+              {
+                  pattern: "Mark a todo item",
+                  callback: function(){}// Function to mark a todo item
+              },
+              {
+                  pattern: "Remove a todo item",
+                  callback: function(){}// Function to handle remove todo item
+              }
+          ]);
+          return;
         }
     }
     convo.say("I couldn't find the card name '"+cardName+"' in your storyboard");
