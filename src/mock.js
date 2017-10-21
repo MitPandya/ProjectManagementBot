ae91c1acd0c63db864dde676eb217b32424c411f
Neha Sak <nsakhal2@ncsu.edu>
2017-10-21 17:23:49 -0400

    Issue #13: REST API handler to fetch card from trello

--- src/mock.js
+++ src/mock.js
@@ -1,23 +1,27 @@
 var nock = require('nock');
-//var request = require('supertest')("http://api.github.com");
-var request = require('request');
 var data = require('./mocks/mock.json');
 
-module.exports.getCheckListItems = function(callback){
+module.exports.startMock = function(){
 
-	//if(process.env.NOCK_ON=="true"){
+	// Mock for story board
+	var pathRegex = /\/1\/members\/me\/boards.*/;
+	nock("https://api.trello.com")
+	.persist()
+	.get(pathRegex)
+	.reply(200, JSON.stringify(data.MeBoards) );
 
-	   nock("http://api.github.com")
-	  .get("/checkListItems")
-	  .reply(200, JSON.stringify(data.checkListItems) );
-	//}
-
-	var option={method:'GET',url:'http://api.github.com/checkListItems'};
-	request.get(option, function(e,r,b){
-		console.log(b);
-		callback(e,r,b);
-	});
+	// Mock for all the lists in a board
+	pathRegex = /\/1\/boards\/.*\/lists/;
+	nock("https://api.trello.com")
+	.persist()
+	.get(pathRegex)
+	.reply(200, JSON.stringify(data.boardLists) );
+	
+	// Mock for getting all the cards in a List
+	pathRegex = /\/1\/batch/;
+	nock("https://api.trello.com")
+	.persist()
+	.get(pathRegex)
+	.reply(200, JSON.stringify(data.cardList) );
 
 };
-
-//getCheckListItems();
---
+++ src/restAPIHelper.js
@@ -0,0 +1,89 @@
+var request = require('request');
+
+
+var api_fetch_story_board = "https://api.trello.com/1/members/me/boards?key={APP_KEY}&token={TOKEN_VALUE}";
+var api_fetch_lists = "https://api.trello.com/1/boards/{BOARD_ID}/lists?key={APP_KEY}&token={TOKEN_VALUE}";
+var api_fetch_cards = "https://api.trello.com/1/batch/?urls={LISTS_IDS}&key={APP_KEY}&token={TOKEN_VALUE}";
+
+// Wrapper to hide underlying callbacks
+module.exports.openCard = function(userID, cardName, convo, callback){
+    loadStoryBoard(userID,cardName, convo,callback);
+}
+
+module.exports.addTodoItem = function(userID, cardID, todoItemName, callback){
+    
+}
+
+function loadStoryBoard(userID, cardName, convo, callback){
+    var urlBoard = api_fetch_story_board.replace("{APP_KEY}",global.APP_KEY).replace("{TOKEN_VALUE}",global.TRELLO_TOKEN_MAP[userID]);
+    var options = { 
+                    method: 'GET',
+                    url: urlBoard  };
+    request(options, function (error, response, body) {
+        // parse response
+        if (error) throw new Error(error);
+        body = JSON.parse(body);
+        
+        loadBoardList(userID,body[0].id,cardName, convo,callback);
+    });
+}
+
+function loadBoardList(userID, boardID,cardName, convo, callback){
+    // load all cards in this board
+    var urlList= api_fetch_lists.replace("{BOARD_ID}",boardID).replace("{APP_KEY}",global.APP_KEY).replace("{TOKEN_VALUE}",global.TRELLO_TOKEN_MAP[userID]);
+
+    var options = { 
+                    method: 'GET',
+                    url: urlList  };
+
+    request(options, function (error, response, body) {
+        // parse response
+        if (error) throw new Error(error);
+        body = JSON.parse(body);
+
+        loadCards(userID,body,cardName, convo,callback);
+
+    });
+}
+
+function loadCards(userID, listArray,cardName, convo,callback){
+    // consider only only first story board
+    var listCount = listArray.length;
+    var batchListInfo = [];
+
+    for(var i=0;i<listCount;i++){
+        batchListInfo.push("/lists/"+listArray[i].id+"/cards");
+    }
+    batchListInfo = batchListInfo.join();
+
+    // load all cards in this board
+    var urlCards= api_fetch_cards.replace("{LISTS_IDS}",batchListInfo).replace("{APP_KEY}",global.APP_KEY).replace("{TOKEN_VALUE}",global.TRELLO_TOKEN_MAP[userID]);
+    var options = { 
+                    method: 'GET',
+                    url: urlCards  };
+    request(options, function (error, response, body) {
+        // parse response
+        if (error) throw new Error(error);
+        
+        body = JSON.parse(body);
+        
+        var respCount = body.length;
+
+        var cardList = [];
+
+        for(var i=0;i<respCount;i++){
+            respCardList = body[i]['200'];
+            for(var j=0;j<respCardList.length;j++){
+                cardList.push({
+                    'id':respCardList[j].id,
+                    'name':respCardList[j].name,
+                    'desc':respCardList[j].desc,
+                    'idChecklists':respCardList[j].idChecklists[0],
+                });
+            }
+        }
+        console.log(callback);
+        callback(convo,cardName, cardList);
+    }); 
+
+}
