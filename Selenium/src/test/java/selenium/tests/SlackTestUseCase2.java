4edd2668ebc2d8422aae342fcfa98d84d258b7fc
dgupta9 <dgupta9@ncsu.edu>
2017-10-24 21:05:00 -0400

    Added selenium test for use case2 issue #51

---
+++ Selenium/src/test/java/selenium/tests/SlackTestUseCase2.java
@@ -0,0 +1,166 @@
+package selenium.tests;
+
+/*
+* References : https://sqa.stackexchange.com/questions/13008/how-to-determine-whether-element-is-clickable-or-not
+* https://www.guru99.com/xpath-selenium.html http://selenium-python.readthedocs.io/locating-elements.html
+*/
+
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class SlackTestUseCase2 {
+
+	private static WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp() throws Exception 
+	{
+		//driver = new HtmlUnitDriver();
+		ChromeDriverManager.getInstance().setup();
+		driver = new ChromeDriver();
+		UtilityFunction();
+	}
+	
+	@AfterClass
+	public static void  tearDown() throws Exception
+	{
+		driver.close();
+		driver.quit();
+	}
+
+	
+	@Test
+	public void useCase2HappyPath() throws InterruptedException
+	{
+
+		// Type something
+		WebElement messageBot = driver.findElement(By.id("msg_input"));
+		assertNotNull(messageBot);
+			
+		Actions actions = new Actions(driver);
+		actions.moveToElement(messageBot);
+		actions.click();
+	
+		actions.sendKeys("Hey");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+
+		WebElement msg = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'How can I help you?']"));
+		assertNotNull(msg);
+		actions.sendKeys("Send notification to members of card");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+		WebElement msg2 = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'Can you provide the card name?']"));
+		assertNotNull(msg2);
+		actions.sendKeys("Card1_NEW");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+		WebElement msg3 = driver.findElement(
+				By.xpath("//span[@class=\"message_body\" and text()=\"Here is the card Card1_NEW with description : This is the first card in a list called 'todo'\"]"));
+		assertNotNull(msg3);
+		Thread.sleep(3 * 1000);
+		WebElement msg4 = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'Enter the message']"));
+		assertNotNull(msg4);
+		Thread.sleep(5 * 1000);
+		actions.sendKeys("Sample memo message");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+		WebElement msg5 = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'I have sent your message to all members of this card.']"));
+		assertNotNull(msg5);
+	}
+	
+	@Test
+	public void useCase3AlternativePath() throws InterruptedException
+	{
+
+		// Type something
+		WebElement messageBot = driver.findElement(By.id("msg_input"));
+		assertNotNull(messageBot);
+			
+		Actions actions = new Actions(driver);
+		actions.moveToElement(messageBot);
+		actions.click();
+	
+		actions.sendKeys("Hey");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+
+		WebElement msg = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'How can I help you?']"));
+		assertNotNull(msg);
+		actions.sendKeys("Send notification to members of card");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+		WebElement msg2 = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'Can you provide the card name?']"));
+		assertNotNull(msg2);
+		actions.sendKeys("CardX");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		Thread.sleep(5 * 1000);
+		WebElement msg3 = driver.findElement(
+				By.xpath("//span[@class=\"message_body\" and text() = \"I couldn't find the card name 'CardX' in your storyboard\"]"));
+		assertNotNull(msg3);
+		Thread.sleep(3 * 1000);
+	}
+	
+	// Utility method to open the given webpage only once
+	public static void UtilityFunction(){
+		driver.get("https://seproject-workspace.slack.com/");
+		// Wait until page loads and we can see a sign in button.
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signin_btn")));
+
+		// Find email and password fields.
+		WebElement email = driver.findElement(By.id("email"));
+		WebElement pw = driver.findElement(By.id("password"));
+
+		// Enter our email and password
+		// If running this from Eclipse, you should specify these variables in the run configurations.
+		email.sendKeys(System.getenv("SLACK_EMAIL_ID"));
+		pw.sendKeys(System.getenv("SLACK_PASSWORD"));
+
+		// Click
+		WebElement signin = driver.findElement(By.id("signin_btn"));
+		signin.click();
+
+		// Wait until we go to general channel.
+		wait.until(ExpectedConditions.titleContains("general"));
+
+		// Switch to #selenium-bot channel and wait for it to load.
+		driver.get("https://seproject-workspace.slack.com" + "/messages/trellobot");
+		wait.until(ExpectedConditions.titleContains("trellobot"));
+	}
+
+}
+
